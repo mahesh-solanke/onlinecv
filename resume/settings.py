76467c9096f5f649ca705d73e25c179d7ebe3f38
Mahesh <maheshsolanke69@gmail.com>
2020-12-15 14:29:19 +0530

    online CV

--- resume/settings.py
+++ resume/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'di!32qox8)d1)(vjxtkw=xr*va77iy(lwm01x*j#q*y1=hn-h0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','onlineresumemks.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','maheshonlineresume.herokuapp.com']
 
 
 # Application definition
